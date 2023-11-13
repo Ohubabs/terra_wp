b99ae26183ca70504a552ddaa5f5ec11dd95b8ea
ohubabs <obakinolugbade@gmail.com>
2023-11-13 13:01:42 -0800

    wordpress practice

---
+++ word-sc.tf
@@ -0,0 +1,36 @@
+resource "kubernetes_storage_class" "wordpress-sc" {
+  metadata {
+    name = "wordpress-sc"
+  }
+  storage_provisioner = "ebs.csi.aws.com"
+  reclaim_policy      = "Retain"
+  volume_binding_mode = "Immediate"
+  parameters = {
+    type = "gp2"
+  }
+}
+
+#resource "kubernetes_storage_class" "maria-sc" {
+ # metadata {
+  #  name = "maria-sc"
+  #}
+  #storage_provisioner = "ebs.csi.aws.com"
+  #reclaim_policy      = "Retain"
+  #volume_binding_mode = "Immediate"
+  #parameters = {
+  #  type = "gp2"
+  #}
+#}
+
+
+resource "kubernetes_storage_class" "mysql-sc" {
+  metadata {
+    name = "mysql-sc"
+  }
+  storage_provisioner = "ebs.csi.aws.com"
+  reclaim_policy      = "Retain"
+  volume_binding_mode = "Immediate"
+  parameters = {
+    type = "gp2"
+  }
+}
---
+++ wordpress.tf
@@ -0,0 +1,21 @@
+resource "helm_release" "wordpress" {
+  name       = "wordpress"
+  namespace = "default"
+  repository = "oci://registry-1.docker.io/bitnamicharts"
+  chart      = "wordpress"
+
+  values = [
+    "${file("word-values.yml")}"
+  ]
+}
+
+resource "helm_release" "mysql" {
+  name       = "mysql"
+  namespace = "default"
+  repository = "oci://registry-1.docker.io/bitnamicharts"
+  chart      = "mysql"
+
+  values = [
+    "${file("mysql-values.yml")}"
+  ]
+}
